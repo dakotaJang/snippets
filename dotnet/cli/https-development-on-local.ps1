a09daa5954dfb58c37ec61b009b1f45acef80d12
dakotaJang <dev@dakotajang.com>
2020-06-28 13:30:26 -0700

    Create https-development-on-local.ps1

---
+++ dotnet/cli/https-development-on-local.ps1
@@ -0,0 +1,3 @@
+# Trust the ASP.NET Core HTTPS development certificate on Windows and macOS
+# https://docs.microsoft.com/en-us/aspnet/core/security/enforcing-ssl?view=aspnetcore-3.1&tabs=visual-studio#trust-the-aspnet-core-https-development-certificate-on-windows-and-macos
+dotnet dev-certs https --trust
